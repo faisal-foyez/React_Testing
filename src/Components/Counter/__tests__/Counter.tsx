8555c7082c1019b7f67e0b9d9580564de7182422
Faisal Islam <faisalislam@Faisals-Mac-mini.local>
2024-02-03 00:32:35 +0600

    exercise2-extra complete

--- src/Components/Counter/__tests__/Counter.tsx
+++ src/Components/Counter/__tests__/Counter.tsx
@@ -1,6 +1,6 @@
 import Counter from '../Counter';
 import {render, fireEvent} from '@testing-library/react';
-
+import '@testing-library/jest-dom/'
 
 beforeEach(()=>{
  document.body.innerHTML = '';
@@ -9,26 +9,21 @@ beforeEach(()=>{
 
 test('counter increments and decrements when the buttons are clicked', () => {
 
+  const {container} = render(<Counter />);
 
- const {container} = render(<Counter />);
-
-
- const [increment, decrement] = container.querySelectorAll('button')
- let message = container.querySelector('.message')?.textContent
-  expect(message).toBe('Current count: 0')
+  const [increment, decrement] = container.querySelectorAll('button')
+  let message = container.querySelector('.message')
+    expect(message).toHaveTextContent('Current count: 0')
 
 
- fireEvent.click(increment);
- message = container.querySelector('.message')?.textContent
- expect(message).toBe('Current count: 1')
+  fireEvent.click(increment);
+  message = container.querySelector('.message')
+  expect(message).toHaveTextContent('Current count: 1')
 
 
- fireEvent.click(decrement);
- message = container.querySelector('.message')?.textContent
- expect(message).toBe('Current count: 0')
-
-
-
+  fireEvent.click(decrement);
+  message = container.querySelector('.message')
+  expect(message).toHaveTextContent('Current count: 0')
 
 })
 
