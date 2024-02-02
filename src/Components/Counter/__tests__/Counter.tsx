bab9eb745ae69d4ae07022ae687b4b68d38b4055
Faisal Islam <faisalislam@Faisals-Mac-mini.local>
2024-02-02 23:48:10 +0600

    complted exercise 2

--- src/Components/Counter/__tests__/Counter.tsx
+++ src/Components/Counter/__tests__/Counter.tsx
@@ -1,50 +1,34 @@
-// import React, { ReactElement } from 'react';
-// import { ReactDOM } from 'react';
-import {createRoot} from 'react-dom/client'
-import {act} from 'react-dom/test-utils';
 import Counter from '../Counter';
+import {render, fireEvent} from '@testing-library/react';
 
 
 beforeEach(()=>{
-  document.body.innerHTML = '';
+ document.body.innerHTML = '';
 })
+
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  const div = document.createElement('div');
-  // 🐨 append the div to document.body (💰 document.body.append)
-  document.body.append(div);
-  // 🐨 use createRoot to render the <Counter /> to the div
-  const root = createRoot(div);
 
-  act(() =>root.render(<Counter />))
-  // 🐨 get a reference to the increment and decrement buttons:
-  const [increment, decrement] = div.querySelectorAll('button')
-  console.log(increment);
-  // 🐨 get a reference to the message div:
-  let message = div.querySelector('.message')?.innerHTML
-  
+
+ const {container} = render(<Counter />);
+
+
+ const [increment, decrement] = container.querySelectorAll('button')
+ let message = container.querySelector('.message')?.textContent
   expect(message).toBe('Current count: 0')
 
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  const incrementClickEvent = new MouseEvent('click', {
-    bubbles:true,
-    cancelable:true,
-    button:0
-  })
-  act(()=>increment.dispatchEvent(incrementClickEvent));
-  message = div.querySelector('.message')?.innerHTML
-  expect(message).toBe('Current count: 1')
-  // 🐨 assert the message.textContent
-  const decrementClickEvent = new MouseEvent('click', {
-    bubbles:true,
-    cancelable:true,
-    button:0
-  })
-  act(()=>decrement.dispatchEvent(decrementClickEvent));
-  message = div.querySelector('.message')?.innerHTML
-  expect(message).toBe('Current count: 0')
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  div.remove();
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+
+ fireEvent.click(increment);
+ message = container.querySelector('.message')?.textContent
+ expect(message).toBe('Current count: 1')
+
+
+ fireEvent.click(decrement);
+ message = container.querySelector('.message')?.textContent
+ expect(message).toBe('Current count: 0')
+
+
+
+
 })
+
