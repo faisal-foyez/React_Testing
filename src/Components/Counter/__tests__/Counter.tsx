c9fb3207616582ffebd636b6274540a9ca37cae8
Faisal Islam <faisalislam@Faisals-Mac-mini.local>
2024-02-03 17:20:09 +0600

    Use userEvent to make the user clicks implementation details free.
    using userEvent from @testing-library/user-event replacing fireEvent to avoid the
    implementation details dependency like when a user click he not only click event happens
    but also mouseenter, mouseleave, keydown, keyup event also occurs. To cover them all
    @testing-library/user-event package is better.

--- src/Components/Counter/__tests__/Counter.tsx
+++ src/Components/Counter/__tests__/Counter.tsx
@@ -1,26 +1,25 @@
 import Counter from '../Counter';
-import {render, screen, fireEvent} from '@testing-library/react';
+import {render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/'
 
-test('counter increments and decrements when the buttons are clicked', () => {
+test('counter increments and decrements when the buttons are clicked', async () => {
 
   render(<Counter />);
 
-  const increment = screen.getByRole('button',{name:/increment/i})
-  const decrement = screen.getByRole('button', {name:/decrement/i})
+  const increment = screen.getByRole('button',{name:/increment/i});
+  const decrement = screen.getByRole('button', {name:/decrement/i});
 
   let message = screen.getByText(/count/i);
-  expect(message).toHaveTextContent('Current count: 0')
+  expect(message).toHaveTextContent('Current count: 0');
 
-
-  fireEvent.click(increment);
+  await userEvent.click(increment);
   message = screen.getByText(/count/i);
-  expect(message).toHaveTextContent('Current count: 1')
+  expect(message).toHaveTextContent('Current count: 1');
 
-
-  fireEvent.click(decrement);
+  await userEvent.click(decrement);
   message = screen.getByText(/count/i);
-  expect(message).toHaveTextContent('Current count: 0')
+  expect(message).toHaveTextContent('Current count: 0');
 
 })
 
