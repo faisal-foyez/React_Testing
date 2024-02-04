434bdbf4991921122e0f94ee459fe468f89a18fe
Faisal Islam <faisalislam@Faisals-Mac-mini.local>
2024-02-04 13:04:56 +0600

    Use @faker-js/faker package to generate random username and password for readability
    For better readability that the username and password doesn't actually matter here
    they are basically randomly generated. For that purpose we are using faker package.

--- src/Components/LoginForm/__tests__/LoginForm.tsx
+++ src/Components/LoginForm/__tests__/LoginForm.tsx
@@ -1,24 +1,30 @@
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import LoginForm from '../LoginForm';
+import {faker} from '@faker-js/faker';
+
+const buildLoginForm = () =>{
+  return{
+    username:faker.internet.userName(),
+    password: faker.internet.password()
+  }
+}
 
 test('submitting the form calls onSubmit with username and password',async ()=>{
 
-  let submittedData;
-  const handleSubmit =  (data:any) => (submittedData = data);
-
+  const handleSubmit =  jest.fn();
   render(<LoginForm onSubmit={handleSubmit}/>);
   
-  const username = 'jamal@gmail.com';
-  const password = 'slkdjflsd34';                                                                                                                                                                                                                                                                                                                                      
+  const {username, password} = buildLoginForm();                                                                                                                                                                                                                                                                                                                                    
 
 
   await userEvent.type(screen.getByLabelText(/username/i), username);
   await userEvent.type(screen.getByLabelText(/password/i), password);
   await userEvent.click(screen.getByRole('button',{name:/submit/i}));
 
-  expect(submittedData).toEqual({
+  expect(handleSubmit).toHaveBeenCalledWith({
     username,
     password
-  })
+  });
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
 })
